bf275b27be127e4aaf45db866f360c213f0877e5
caelansar <819711623@qq.com>
2019-07-20 16:55:44 +0800

    change namej

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ import hello_pb2_grpc
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:  # create channel
         stub = hello_pb2_grpc.GreeterStub(channel)  # create stub to call service methods
-        response = stub.SayHello(hello_pb2.HelloRequest(name='niconiconi'))  # call service methods
+        response = stub.SayHello(hello_pb2.HelloRequest(name='cae'))  # call service methods
         print("Greeter client received: " + response.message)
 
 
